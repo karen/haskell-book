b0c9f2dfe03ee4b26d62112f46e3607575d334db
Karen Ang <thekarenang@gmail.com>
2017-12-10 01:26:33 +0800

    Updated Cipher

--- ch11/Cipher.hs
+++ ch11/Cipher.hs
@@ -14,7 +14,7 @@ unCaesar n x = caesarHelper n x Decrypt
 caesarHelper :: Int -> String -> CipherType -> String
 caesarHelper _ [] _ = []
 caesarHelper n (x:xs) ctype = chr (ord base + (mod (offset ctype x base n) 26)) : caesarHelper n xs ctype where
-    base = if compare x 'a' == GT then 'a' else 'A'
+    base = if compare x 'a' /= LT then 'a' else 'A'
 
 offset :: CipherType -> Char -> Char -> Int -> Int
 offset Encrypt x base n = (+) (ord x - ord base) n
@@ -26,6 +26,12 @@ vigenere c x = go 0 c x where
     go i c (' ':xs) = ' ' : go (mod (i) (length c)) c xs
     go i c (x:xs) = (caesar (ord (c !! i) - ord base) [x]) ++ go (mod (i+1) (length c)) c xs where
         base = if compare x 'a' == GT then 'a' else 'A'
+
+main :: IO ()
+main = do
+    word <- getLine
+    rotation <- getLine
+    putStrLn $ caesar (read rotation :: Int) word
 
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf [] _ = True
