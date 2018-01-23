0f78c8bc4d21ec9c62a7747abe097eff178aadd5
Karen Ang <thekarenang@gmail.com>
2018-01-23 23:09:38 +0800

    Chapter 25: Monad Transformers Part 1

---
+++ ch25/Intermission.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE InstanceSigs #-}
+
+import Control.Applicative(liftA2)
+
+newtype Compose f g a =
+    Compose { getCompose :: f (g a) }
+    deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+    fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) =>
+         Applicative (Compose f g) where
+    pure :: a -> Compose f g a
+    pure x = Compose $ (pure (pure x))
+
+    (<*>) :: Compose f g (a -> b)
+          -> Compose f g a
+          -> Compose f g b
+    (Compose fgab) <*> (Compose fga) = Compose $ (fmap (<*>) fgab) <*> fga
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+    foldMap :: (Monoid m) => (a -> m)
+            -> Compose f g a
+            -> m
+    foldMap am (Compose fga) = (foldMap . foldMap) am fga
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+    traverse ahb (Compose fga) = Compose <$> (traverse(traverse ahb) fga)
+
+class Bifunctor p where
+    {-# MINIMAL bimap | first, second #-}
+
+    bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+    bimap f g = first f . second g
+
+    first :: (a -> b) -> p a c -> p b c
+    first f = bimap f id
+
+    second :: (b -> c) -> p a b -> p a c
+    second = bimap id
+
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+    bimap f g (Deux a b) = Deux (f a) (g b)
+
+data Const a b = Const a
+
+instance Bifunctor Const where
+    bimap f _ (Const a) = Const (f a)
+
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+    bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+    bimap f _ (SuperDrei a b) = SuperDrei a (f b)
+
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+    bimap _ _ (SemiDrei a) = SemiDrei a
+
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+    bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+data Either' a b = Left' a | Right' b
+
+instance Bifunctor Either' where
+    bimap f _ (Left' a) = Left' (f a)
+    bimap _ g (Right' b) = Right' (g b)
